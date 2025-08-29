55af46cd62b5356fb83107bcbd1bf189c2b0272b
KentXStuuki <mequiambao@outlook.com>
2025-08-29 15:00:44 +0800

    Finished intro and about section

--- public/src/index.php
+++ public/src/index.php
@@ -13,6 +13,18 @@
     <title>Bahile</title>
 </head>
 <body>
+    <!-- Navbar -->
+    <?php include 'components/navbar.php'?>
     
+    <!-- Introduction Section -->
+    <section id="intro" style="overflow: hidden;">
+        <?php include 'components/intro.php'?>
+    </section>
+
+    <!-- About Section -->
+    <section id="About">
+        <?php include 'components/about.php'?>
+    </section>
+
 </body>
 </html>
