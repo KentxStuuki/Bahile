361c66a8743479c0b296c3eb4e2d4f5cbba8916c
KentXStuuki <mequiambao@outlook.com>
2025-08-29 09:50:20 +0800

    Initial Commit With file structure fixed and scss and css with js connectivity being added

---
+++ public/src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Css And Scass Connection -->
+    <link rel="stylesheet" href="../../public/main/style/main.css">
+
+    <!-- Java Script Connection -->
+    <script src="../../public/main/scripts/bootstrap.bundle.min.js"></script> 
+
+    <title>Bahile</title>
+</head>
+<body>
+    
+</body>
+</html>
