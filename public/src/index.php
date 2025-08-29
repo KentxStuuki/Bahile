5aa85f6fa2a77e3f38c32b763777124db453d000
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-08-29 14:02:52 +0800

    project_component

--- public/src/index.php
+++ public/src/index.php
@@ -6,13 +6,19 @@
 
     <!-- Css And Scass Connection -->
     <link rel="stylesheet" href="../../public/main/style/main.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.theme.default.min.css">
 
     <!-- Java Script Connection -->
     <script src="../../public/main/scripts/bootstrap.bundle.min.js"></script> 
-
+    <script src="../../public/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../../public/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+    <script src="../main/scripts/data.js"></script>
     <title>Bahile</title>
 </head>
 <body>
-    
+    <section id="project-components">
+        <?php include 'project_components.php'; ?>
+    </section>
 </body>
 </html>
